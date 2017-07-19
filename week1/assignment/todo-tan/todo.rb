c69300c0173c26b7fbcec308c423d73193f8ea5b
Tân Bùi <hoangtan223@gmail.com>
2017-07-19 22:38:44 +0700

    Reindent todo.rb

--- week1/assignment/todo-tan/todo.rb
+++ week1/assignment/todo-tan/todo.rb
@@ -38,62 +38,63 @@ class Todo
       6. "remove 2" will remove the 2nd item in the list
       6. "save" will save the todo list to file
       7. "show trash" will show deleted tasks
-      8. "exit" to exit.}
-    end
-    
-    def mark_done_at(index)
-      @list.mark_done_at(index)
-      seperate("Mark \"#{@list.items[index].name}\" as done!")
-    end
-    
-    def remove_at(index)
-      item_to_delete = @list.items[index]
-      @trash_list.add(item_to_delete)
-      @last_request = {action: 'remove', item: item_to_delete}
-      @list.remove_at(index)
-      seperate("Remove \"#{item_to_delete.name}\" successful!!!")
-    end
-    
-    def undo_remove
-      last_item = @last_request[:item]
-      @list.add(last_item)
-      @trash_list.remove_by_name(last_item.name)
-      @last_request = {}
-      seperate("Undo remove action!")
-    end
-    
-    def undo_add
-      index_to_remove = @list.items.index{|item| item.name == @last_request[:item_name]}
-      @list.remove_at(index_to_remove)
-      seperate("Undo add action!")
-      @last_request = {}
-    end
-    
-    def undo
-      if @last_request == {}
+      8. "exit" to exit.
+    }
+  end
+  
+  def mark_done_at(index)
+    @list.mark_done_at(index)
+    seperate("Mark \"#{@list.items[index].name}\" as done!")
+  end
+  
+  def remove_at(index)
+    item_to_delete = @list.items[index]
+    @trash_list.add(item_to_delete)
+    @last_request = {action: 'remove', item: item_to_delete}
+    @list.remove_at(index)
+    seperate("Remove \"#{item_to_delete.name}\" successful!!!")
+  end
+  
+  def undo_remove
+    last_item = @last_request[:item]
+    @list.add(last_item)
+    @trash_list.remove_by_name(last_item.name)
+    @last_request = {}
+    seperate("Undo remove action!")
+  end
+  
+  def undo_add
+    index_to_remove = @list.items.index{|item| item.name == @last_request[:item_name]}
+    @list.remove_at(index_to_remove)
+    seperate("Undo add action!")
+    @last_request = {}
+  end
+  
+  def undo
+    if @last_request == {}
       puts "Nothing to undo"
     else
       case @last_request[:action]
-    when 'remove'
-      undo_remove
-    when 'add'
-      undo_add
+      when 'remove'
+        undo_remove
+      when 'add'
+        undo_add
+      end
     end
   end
-end
 
-def start
-  loop do       
-    if prompt == 'exit'
-      puts "Goodbye!"
-      break
+  def start
+    loop do       
+      if prompt == 'exit'
+        puts "Goodbye!"
+        break
+      end
     end
   end
-end
 
-def prompt
-  seperate("Tan's Todo app")
-  puts "Hi grandma! what do you want me to do?"
+  def prompt
+    seperate("Tan's Todo app")
+    puts "Hi grandma! what do you want me to do?"
     user_input = gets.chomp
     handle_user_request(user_input)
     user_input
@@ -109,34 +110,34 @@ def prompt
   def handle_user_request(request)
     seperate(request)
     case 
-  when request == 'help'
-    help
-  when request == 'all'
-    @list.display
-  when request == 'show undone'
-    @list.display_undone
-  when request == 'show done'
-    @list.display_done
-  when request[0] == '+'
-    add(request[1..-1].strip)
-  when request[0..5] == 'remove'
-    remove_at(request[6..-1].to_i - 1)
-  when request[0..3] == "save"
-    if request.length == 4
-      save
+    when request == 'help'
+      help
+    when request == 'all'
+      @list.display
+    when request == 'show undone'
+      @list.display_undone
+    when request == 'show done'
+      @list.display_done
+    when request[0] == '+'
+      add(request[1..-1].strip)
+    when request[0..5] == 'remove'
+      remove_at(request[6..-1].to_i - 1)
+    when request[0..3] == "save"
+      if request.length == 4
+        save
+      else
+        save(request[4..-1].strip)
+      end
+    when request == 'undo'
+      undo
+    when request[0..3] == "done"
+      mark_done_at(request[4..-1].to_i - 1)
+    when request == "show trash"
+      @trash_list.display
     else
-      save(request[4..-1].strip)
+      puts "Sorry I don't understand your command!!!"
     end
-  when request == 'undo'
-    undo
-  when request[0..3] == "done"
-    mark_done_at(request[4..-1].to_i - 1)
-  when request == "show trash"
-    @trash_list.display
-  else
-    puts "Sorry I don't understand your command!!!"
   end
-end
 end
 
 @todo = Todo.new
