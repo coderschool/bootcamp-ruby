94894814ba2bf6b811bc9fe2d12005bbaf46611d
Harley Trung <harley@coderschool.vn>
2017-07-19 21:20:52 +0700

    end of lecture 2-3

---
+++ week2/lab2-3/README.md
@@ -0,0 +1,11 @@
+# Lab2-3b
+
+This is a simple multi-page sinatra app.
+
+```ruby
+get "/about" do
+  "about me"
+end
+```
+
+You can also inline `code` like that.
---
+++ week2/lab2-3/app.rb
@@ -0,0 +1,22 @@
+require "sinatra"
+
+get "/" do
+  # this will look for the views/index.html.erb file
+  erb :"index.html", locals: {name: "CoderSchool"}, layout: :"layout.html"
+end
+
+get "/about" do
+  erb :"home/about.html", locals: {name: "CoderSchool"}, layout: :"layout.html"
+end
+
+get "/trello" do
+  my_items = [
+    {name: "Learn Strings", done: false},
+    {name: "Learn Hash", done: true},
+    {name: "Learn Variables", done: true},
+    {name: "Learn Methods", done: true},
+    {name: "Learn ERB", done: true},
+    {name: "Learn Partials", done: true},
+  ]
+  erb :"trello.html", layout: :"layout.html", locals: {items: my_items}
+end
