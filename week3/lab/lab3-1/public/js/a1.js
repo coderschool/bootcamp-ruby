32a09f56143be7011e2dd2168e5e67df79e133d9
Tân Bùi <hoangtan223@gmail.com>
2017-07-31 02:03:52 +0700

    Finish week 3 assignment

---
+++ week3/lab/lab3-1/public/js/a1.js
@@ -0,0 +1,2 @@
+console.log("you should see this in the Inspect console");
+alert("hello i am from a1.js");
---
+++ week3/lab/lab3-1/public/js/c1.js
@@ -0,0 +1,5 @@
+var items = document.getElementsByClassName("item");
+// let's print out a few values to the Inspect Console
+console.log("items: ", items);
+console.log("item 0: ", items[0]);
+console.log("item 0's innerHTML: ", items[0].innerHTML);
